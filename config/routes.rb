2112a7a1f542fed87bb939b6d5d2264e53d46459
Rafael Adrian <rafael.avendano12317@gmail.com>
2019-03-20 21:47:54 -0600

    Mostrando_productos_en_Front

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'producto/index'
+
   get '/hola', to: 'usuario#index'
   get '/mostrar', to: 'usuario#mostrar'
+
+  #Products
+  get '/chamarra', to: 'producto#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
