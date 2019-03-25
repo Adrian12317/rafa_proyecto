7339ebc6301cb8df14b0c9a4636a1a9aa5462b07
Rafael Adrian <rafael.avendano12317@gmail.com>
2019-03-24 21:23:11 -0600

    Avance inicio

--- app/controllers/producto_controller.rb
+++ app/controllers/producto_controller.rb
@@ -28,31 +28,6 @@ class ProductoController < ApplicationController
         @cham_tienda = ult_chamarra.tienda
          @cham_promotor = ult_chamarra.promotor 
 
-
-         #Segunda chamarra
-
-
-         nuevo2 = Product.new
-         nuevo2.nombre = "Chamarra Nike Sb Coaches Rompevientos (roja)"
-         nuevo2.codigo = "12317a"
-         nuevo2.precio = 1220.00
-         nuevo2.stock = 15
-         nuevo2.existencia = true
-         nuevo2.tienda = "Nike Norte"
-         nuevo2.promotor = "Nike SB"
-
-         nuevo2.save
-
-          nsb = Product.nuevo2
-
-           @cham2_nombre = nsb.nombre
-          @nsb2_codigo = nsb.codigo
-           @nsb2_precio = nsb.precio
-            @nsb2_stock = nsb.stock
-             @nsb2_existencia = nsb.existencia
-              @nsb2_tienda = nsb.tienda
-               @nsb2_promotor = nsb.promotor
-
           
   end
 end
